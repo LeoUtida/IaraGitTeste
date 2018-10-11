86afa96d304676ae851fb7112c92dd2d82d1c580
LeoUtida <leoutida@gotmail.com>
2018-10-11 19:34:21 -0300

    Adicionando arquivos iniciais

---
+++ funcoes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funcoes.h"
+
+float soma3nums(float a, float b, float c){
+	float resultado = a + b + c;
+	return resultado;
+}
+
+float mult3nums(float a, float b, float c){
+	return (a * b * c);
+}
---
+++ funcoes.h
@@ -0,0 +1,7 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+float soma3nums(float, float, float);
+float mult3nums(float, float, float);
+
+#endif
