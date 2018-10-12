2f05cbc92baf76ddafd7d4a3a746f1982abd72bd
LeoUtida <leoutida@gotmail.com>
2018-10-11 23:18:46 -0300

    Execicio 1 feito com header

--- lista1.c
+++ lista1.c
@@ -10,6 +10,57 @@
 #include "funcoes.h"
 
 int main(int argc, char *argv[]){
-	float result = mult3nums(4.0, 2.0, 3.0);
-	printf("Resultado = %f\n", result);
+
+	float a,b,c;
+	float peso1,peso2,peso3;
+
+	printf("#########################\n");
+	printf("Exercicio1: Soma\n");
+	printf("Digite o valor do primeiro elemento: ");
+	scanf("%f", &a);
+	printf("Digite o valor do segundo elemento: ");
+	scanf("%f", &b);
+	printf("Digite o valor do terceiro elemento: ");
+	scanf("%f", &c);
+
+	printf("Resultado = %f\n\n", soma3nums(a,b,c));
+
+	printf("#########################\n");
+	printf("Exercicio1: Multiplicacao\n");
+	printf("Digite o valor do primeiro elemento: ");
+	scanf("%f", &a);
+	printf("Digite o valor do segundo elemento: ");
+	scanf("%f", &b);
+	printf("Digite o valor do terceiro elemento: ");
+	scanf("%f", &c);
+
+	printf("Resultado = %f\n\n", mult3nums(a,b,c));
+
+	printf("#########################\n");
+	printf("Exercicio1: Divisao tratando 0\n");
+	printf("Digite o valor do numerador: ");
+	scanf("%f", &a);
+	printf("Digite o valor do denominador: ");
+	scanf("%f", &b);
+
+	printf("Resultado = %f\n\n", divTrata0(a,b));
+
+	printf("#########################\n");
+	printf("Exercicio1: Media com pesos\n");
+	printf("Digite o valor do primeiro elemento: ");
+	scanf("%f", &a);
+	printf("Digite o valor do segundo elemento: ");
+	scanf("%f", &b);
+	printf("Digite o valor do terceiro elemento: ");
+	scanf("%f", &c);
+	printf("Digite o valor do primeiro peso: ");
+	scanf("%f", &peso1);
+	printf("Digite o valor do segundo peso: ");
+	scanf("%f", &peso2);
+	printf("Digite o valor do terceiro peso: ");
+	scanf("%f", &peso3);
+
+	printf("Resultado = %f\n\n", mediaPesos(a,peso1,b,peso2,c,peso3));
+
+
 }
